c4bda55d59c339d1d8f533952f1bdc8eab9aee13
Justtrymili <99335440+Justtrymili@users.noreply.github.com>
2022-02-17 22:14:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import '/style.css';
 import * as THREE from 'https://cdn.skypack.dev/three';
-import { OrbitControls } from './three/examples/js/controls/OrbitControls';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 
