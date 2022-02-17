035c19c158a206713683fb7431c1e51f80ee66ef
Justtrymili <99335440+Justtrymili@users.noreply.github.com>
2022-02-17 22:24:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -115,8 +115,7 @@ var messages = document.getElementById("messages");
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
 
-button.addEventListener("click",function(){
-  var newMesages = document.createElement("li");
+button.addEventListener("click",function(){var newMesages = document.createElement("li");
   newMesages.innerHTML = textbox.value;
   messages.appendChild(newMesages);
   textbox.value= "";
