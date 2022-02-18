6f512114194606643105d9e520ee103deedaaa10
Justtrymili <99335440+Justtrymili@users.noreply.github.com>
2022-02-18 19:41:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import  styles from '/style.css';
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
+const styles = await import('./style.css', {
+  assert: { type: 'css' }
+});
+document.adoptedStyleSheets = [styles.default];
+
 // Setup
 
 const scene = new THREE.Scene();
