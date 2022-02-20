67d029d12136b8f14939fd0525ae270fc5647abc
Justtrymili <99335440+Justtrymili@users.noreply.github.com>
2022-02-20 16:09:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import  '/style.css';
+import  'https://github.com/Justtrymili/deploying-vite-project-example/blob/main/style.css';
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
